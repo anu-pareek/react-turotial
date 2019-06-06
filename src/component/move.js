4229cc58f11fa69633513d8e9f3e15b848460451
Anuraj Pareek <anupareek@deloitte.com>
2019-06-06 13:28:08 +0530

    push before final lifting up

---
+++ src/component/move.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Move extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Move
